c4588384736efdc92c7ef5311ac51073a0500861
SzymonCiciala <47600083+s-ciciala@users.noreply.github.com>
2022-01-18 17:50:57 +0000

    Create install.sh

---
+++ install.sh
@@ -0,0 +1,15 @@
+conda install -c conda-forge opencv
+conda install numpy scipy matplotlib
+conda install -c conda-forge pbzip2 pydrive
+conda install pillow tqdm
+pip install GPUtil
+conda install pytorch torchvision cudatoolkit=9.0 -c pytorch
+conda install pandas
+conda install tqdm
+conda install future
+conda install joblib
+conda install pyyaml
+conda install sentencepiece
+conda install tb-nightly
+conda install torchaudio
+conda install matplotlib
